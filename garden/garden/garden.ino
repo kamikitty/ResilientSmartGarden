6df636b251d8d8570b3af84e9366144539e75e6a
jasonplojo <jason.plojo@gmail.com>
2017-10-01 11:21:16 -0700

    Added handshake for garden and Android. #41 #42 - WebServer added to Garden to listen for POST request handshake - WebServer will return MAC Address to identify Garden - Added combined WiFi Scan and POST request for garden handshake on Android

--- garden/garden/garden.ino
+++ garden/garden/garden.ino
@@ -5,23 +5,38 @@
 
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+#include <ESP8266WebServer.h>
 #include <ArduinoJson.h>
 
 #include <Wire.h>
 #include "credentials.h"
 
 #define WIFI_DELAY 500 // time to retry connection in ms
+#define SCAN_DELAY 250 // Scanner delay
 #define UPDATE_RATE 1000 * 60 // frequency of sending POST request in ms
 #define PLANTER1_CHANNEL 0 // i2c channel for planter 1
 #define PLANTER2_CHANNEL 1 // i2c channel for planter 2
 
+#define LOCAL_PORT 3001 // Port for WiFi Scanner
+
+// Timers
+long lastPostRequest;
+long lastScanRequest;
+
 // Clients
 WiFiClient client;
+
+// WebServer
+ESP8266WebServer server(LOCAL_PORT);
 
 /*
  * \brief Initializes i2c communication with the garden and WiFi connection to the server
  */
 void setup() {
+  // Initialize timers
+  lastPostRequest = 0;
+  lastScanRequest = 0;
+  
   // Start i2c communication as master
   Wire.begin();
 
@@ -46,7 +61,6 @@ void setup() {
   while (WiFi.status() != WL_CONNECTED){
     delay(500);
     Serial.print(".");
-    Serial.println(WiFi.status());
   }
 
   Serial.println("OK!");
@@ -54,7 +68,11 @@ void setup() {
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
 
+  // Initialize endpoints
+  server.on("/handShake", handleHandShake);
+
   digitalWrite(D0, LOW);
+  server.begin();
 }
 
 /**
@@ -62,15 +80,26 @@ void setup() {
  */
 void loop() {
   // TODO: Check to see if WiFi is still connected. If not, retry to connect
+  long currentMillis = millis();
 
-  digitalWrite(D4, LOW);
-  Serial.println("--Planter 1--");
-  postReadings(PLANTER1_CHANNEL, String(SERVER_URI_PLANTER1));
-  Serial.println("--Planter 2--");
-  postReadings(PLANTER2_CHANNEL, String(SERVER_URI_PLANTER2));
-  digitalWrite(D4, HIGH);
-  delay(UPDATE_RATE);
-  
+  // Send POST request to server every update rate
+  if (currentMillis - lastPostRequest > UPDATE_RATE) {
+    digitalWrite(D4, LOW);
+    Serial.println("--Planter 1--");
+    postReadings(PLANTER1_CHANNEL, String(SERVER_URI_PLANTER1));
+    Serial.println("--Planter 2--");
+    postReadings(PLANTER2_CHANNEL, String(SERVER_URI_PLANTER2));
+    digitalWrite(D4, HIGH);
+    lastPostRequest = currentMillis;
+  }
+
+  // Handle HTTP requests
+  if (currentMillis - lastScanRequest > SCAN_DELAY) {
+    digitalWrite(D0, LOW);
+    server.handleClient();
+    lastScanRequest = currentMillis;
+    digitalWrite(D0, HIGH);
+  }
 }
 
 /**
@@ -156,5 +185,64 @@ float i2cRead(){
   toRead = toRead | Wire.read();
 
   return toRead / 100.0;
+}
+
+/**
+ *  \brief A WebServer handler that checks the incoming POST request for valid handshake
+ *  
+ *  This WebServer handler is assigned to the endpoint "/handShake". It will receive a JSON
+ *  object containing the key "handShake". The value must match the handshake signature. If
+ *  the value matches the handshake, then return a JSON object with validation true and the 
+ *  MAC address of the WiFi Module. If the value does not match, then return a JSON object with
+ *  validation faluse.
+ *  
+ *  @return The server response of the handshake
+ */
+void handleHandShake() {
+  // Build JSON object for receiving POST request
+  StaticJsonBuffer<200> request;
+  JsonObject &jsonRequest = request.parseObject(server.arg("plain"));
+
+  // Build JSON object for server response
+  StaticJsonBuffer<200> response;
+  JsonObject &jsonResponse = response.createObject();
+
+  String serverResponse;
+
+  // If the handshake matches the key...
+  if (jsonRequest["handShake"] == HANDSHAKE) {
+    //...get the MAC address and convert it to a string
+    byte mac[6];
+    WiFi.macAddress(mac);
+
+    String macAddress;
+    
+    macAddress.concat(String(mac[0], HEX));
+    macAddress.concat(":");
+    macAddress.concat(String(mac[1], HEX));
+    macAddress.concat(":");
+    macAddress.concat(String(mac[2], HEX));
+    macAddress.concat(":");
+    macAddress.concat(String(mac[3], HEX));
+    macAddress.concat(":");
+    macAddress.concat(String(mac[4], HEX));
+    macAddress.concat(":");
+    macAddress.concat(String(mac[5], HEX));
+
+    // Insert the data into the JSON object
+    jsonResponse["valid"] = true;
+    jsonResponse["mac"] = macAddress;
+
+    // Convert the JSON object to a string
+    jsonResponse.printTo(serverResponse);
+  } else {
+    //... otherwise set validation to false
+    jsonResponse["valid"] = false;
+    
+    jsonResponse.printTo(serverResponse);
+  }
+
+  // Send server response
+  server.send(200, "application/json", serverResponse);
 }
 
