7eb3f89741be2fb6c4d8a4b1eecfee123ebc7ad7
MARI\Mari S <marinela.sanchez88@gmail.com>
2017-10-03 16:19:54 -0700

    Offically not empty. I promise.

---
+++ web/RSG/client/src/app/contact.ts
@@ -0,0 +1,10 @@
+//usr schema
+
+export class User{
+	_id: string;
+	firstName:String;
+	lastName:String;
+	userName:String;
+	email:String;
+	passWord:String;
+}
---
+++ web/RSG/client/src/app/user.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import {User} from './user';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class UserService {
+
+  constructor(private http: Http) { }
+
+  //retrieve contacts
+
+  getUsers()
+  {
+  	return this.http.get('http://localhost:3000/api/user').map(res => res.json());
+  }
+
+  //add user
+  addUser(newContact)
+  {
+  	var headers = new Headers();
+  	headers.append('Content-Type', 'application/json');
+  	return this.http.post('http://localhost:3000/api/user', newUser, {headers:headers}).map(res => res.json());
+  }
+  //delete method
+  deleteContact(id)
+  {
+  	return this.http.delete('http://localhost:3000/api/contact/'+id).map(res=> res.json());
+  }
+
+}
